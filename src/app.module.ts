e0e92e8b6b9130e3321878ed0111f7133ddead0b
Nickita <102178609+manihateu@users.noreply.github.com>
2025-01-17 23:40:21 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -21,7 +21,7 @@ import { RecomendationModule } from './recomendation/recomendation.module';
     BotModule,
     CategoriesModule,
     CoursesModule,
-    RecomendationModul
+    RecomendationModule
   ],
 })
 export class AppModule {}
