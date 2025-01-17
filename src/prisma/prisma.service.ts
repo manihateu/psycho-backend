ebdd5ffa711ada9a659a38737c6a1a2489cde7ed
Nickita <102178609+manihateu@users.noreply.github.com>
2025-01-18 00:27:24 +0300

    change db

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -10,7 +10,7 @@ export class PrismaService
     super({
       datasources: {
         db: {
-          url: 'postgres://neondb_owner:jB1HFo9TyPVn@ep-proud-hall-a2dz761y-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require',
+          url: process.env.DATABASE_URL,
         },
       },
     });
